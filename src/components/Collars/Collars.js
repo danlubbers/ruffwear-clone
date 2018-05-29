f04a7190dae70a282d9b2be080babfc270e1476c
Steven <stevenwright1012@gmail.com>
2018-05-29 15:30:53 -0600

    added all view components and .scss files and added to router

---
+++ src/components/Collars/Collars.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Collars extends React.Component{
+    render(){
+        return(
+            <div>
+                Collars
+            </div>
+        )
+    }
+}
+
+export default Collars
