0eb8774382d4e60d12c8c5ec2087fa6c3bef0dd4
Steven <stevenwright1012@gmail.com>
2018-05-29 14:56:20 -0600

    added home and leashes component

--- src/router.js
+++ src/router.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
-import Harnesses from './components/Harnesses'
+import Home from './components/Home/Home'
+import Harnesses from './components/Harnesses/Harnesses';
+import Leashes from './components/Leashes/Leashes'
 
     export default(
         <Switch>
-            <Route exact path="/" component={Harnesses}/>            
+            <Route exact path="/" component={Home}/>            
             <Route path="/harnesses" component={Harnesses}/>
+            <Route path="/leashes" component={Leashes}/>            
         </Switch>
     )
