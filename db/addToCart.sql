5c1fee68d9bffd43676f41b58470b3067fe59cef
Steven <stevenwright1012@gmail.com>
2018-05-30 10:30:50 -0600

    -

--- db/addToCart.sql
+++ db/addToCart.sql
@@ -1,5 +1,6 @@
 INSERT INTO cart (product_id, qty, user_id) 
 VALUES ($1, 1, $2);
+
 SELECT cart.user_id, products.title, products.price, products.product_img, cart.product_id, cart.qty
 FROM cart
 FULL OUTER JOIN products ON cart.product_id=products.id
