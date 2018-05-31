fa1462681010d3f912ed0b8accc0060aa87d9db3
Dan Lubbers <lubbers.dan@gmail.com>
2018-05-31 11:17:32 -0600

    more header and beginning of homepage body is being added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import router from './router'
 import './styles/App.css'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 class App extends Component {
   render() {
@@ -12,7 +13,9 @@ class App extends Component {
         </div>
 
           {router}
-
+        <div className='footer'>
+           <Footer/>
+        </div>
       </div>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+// Image imports
+
 class Home extends React.Component{
     render(){
         return(
-        <div>
-           
+        <div className='home-container'>
+            <div className='featured-photo'>
+                <div className='featured-text-container'>
+                    <h1 className='mile-text'>BE IN THE MILE</h1>
+                    <div className='button-container'>
+                        <button className='adventureBtn'>Start Adventure</button>
+                        <button className='shopCollectionBtn'>Shop Collection</button>
+
+                    </div>
+
+                </div>
+            </div>
            
          </div>
         )
