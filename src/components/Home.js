01ce13236d02fb81e8e64b07ec730ba4c20b21ce
Heather Flaten <hflatn@gmail.com>
2018-05-31 10:25:53 -0600

    Being forced to commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ class Home extends React.Component{
             // Header
             <div>
                 <Header/>
-                
+                <Footer/>
             </div>
         )
     }
