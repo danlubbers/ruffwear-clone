ca4bcd17805248c2eec4896d39130b4d1b193089
Dan Lubbers <lubbers.dan@gmail.com>
2018-06-04 13:43:19 -0600

    hover functionality to the header drop downs

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,21 +22,21 @@ class Header extends Component {
             showShop: false,
             showTails: false,
             showAbout: false,
-            // selected: '',
-            // images: [
-            //     harnesses = 'dummy-data',
-            //     leashes = 'dummy-data',
-            //     apparel = 'dummy-data',
-            //     lifeJackets = 'dummy-data',
-            //     collars = 'dummy-data',
-            //     boots = 'dummy-data',
-            //     packs = 'dummy-data', 
-            //     bowls = 'dummy-data',
-            //     safety = 'dummy-data',
-            //     beds = 'dummy-data',
-            //     toys = 'dummy-data',
-            //     giftcards = 'dummy-data'
-            // ]
+            selected: 'harnesses',
+            images: {
+                harnesses: Harness,
+                leashes: 'dummy-data',
+                apparel: 'dummy-data',
+                lifeJackets: 'dummy-data',
+                collars: 'dummy-data',
+                boots: 'dummy-data',
+                packs: 'dummy-data', 
+                bowls: 'dummy-data',
+                safety: 'dummy-data',
+                beds: 'dummy-data',
+                toys: 'dummy-data',
+                giftcards: 'dummy-data'
+            }
         }
         this.handleClickHome = this.handleClickHome.bind(this);
         this.handleClickShop = this.handleClickShop.bind(this);
@@ -72,13 +72,19 @@ class Header extends Component {
         })
     }
 
+    handleMouseOver(event) {
+        console.log(event.target)
+        console.log(event.target.id)
+        this.setState({selected: event.target.id});
+    }
+
     render() {
         let { showShop, showTails, showAbout } = this.state;
         let slideCssShop = showShop ? 'slide-menu slide-menu-position' : 'slide-menu';
         let slideCssTails = showTails ? 'slide-menu slide-menu-position' : 'slide-menu';
         let slideCssAbout = showAbout ? 'slide-menu slide-menu-position' : 'slide-menu';
 
-        // let currentImage = this.state.images[this.state.selected];
+        let currentImage = this.state.images[this.state.selected];
 
         // console.log(slideCssShop)
         // console.log(slideCssTails)
@@ -109,7 +115,7 @@ class Header extends Component {
                 <div className={slideCssShop}>
                     <div className='shop-container'>
                         <div className='container-left'>
-                            <img className='harness-image' src={Harness} alt='harness' />
+                            <img className='harness-image' src={currentImage} alt='harness' />
                         </div>
 
                         <div className='container-right'>
@@ -118,9 +124,8 @@ class Header extends Component {
                                     <h3>GEAR</h3>
                                     <div className='gear-container'>
                                         <div className='gear-split-left'>
-                                            <Link to='/collections/harnesses'  onClick={this.handleClickShop} 
-                                            // onMouseOver={currentImage}
-                                            ><li>Harnesses</li> </Link>
+                                            <Link to='/collections/harnesses' onClick={this.handleClickShop} 
+                                            ><li onMouseOver={e=>this.handleMouseOver(e)} id='harnesses'>Harnesses</li> </Link>
                                             <Link to='/collections/leashes' onClick={this.handleClickShop}><li>Leashes</li> </Link>
                                             <Link to='/collections/collars' onClick={this.handleClickShop}><li>Collars</li> </Link>
                                             <li>Apparel</li>
