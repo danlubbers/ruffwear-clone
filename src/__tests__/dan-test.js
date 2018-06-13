945b3054a2473035d11c4e833c039ab090cd3e8d
Dan Lubbers <lubbers.dan@gmail.com>
2018-06-13 16:11:21 -0600

    jest tests

--- src/__tests__/dan-test.js
+++ src/__tests__/dan-test.js
@@ -1,9 +1,9 @@
 const fns = require('../utilities/dan-functions');
 
-// test('validateClickHome should always be false', () => {
-//     let result = fns.validateClickHome(false);
-//     expect(result).toBefalsy();
-// });
+test('validateClickHome should always be false', () => {
+    let result = fns.validateClickHome();
+    expect(result).toBe(false);
+});
 
 test('Returns the category Leashes with 14 products in Array', () => {
     return fns.fetchCategoryLeashes()
@@ -17,8 +17,14 @@ test('Returns the category Harnesses with 12 products in Array', () => {
         expect(res.length).toBe(12)})
 })
 
-test('Returns the category Boots with 8 products in Array', () => {
+test('Returns the category Boots with 7 products in Array', () => {
     return fns.fetchCategoryBoots()
     .then(res => {
         expect(res.length).toBe(7)})
+})
+
+test('Returns the category packs with 9 products in Array', () => {
+    return fns.fetchCategoryPacks()
+    .then(res => {
+        expect(res.length).toBe(9)})
 })
