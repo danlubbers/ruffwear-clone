9708a9b2b0ec53d440477e14f4ad9f078b6f3561
emmadvlpr <emma.phommatha@gmail.com>
2018-06-13 16:05:49 -0600

    update contact&css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import CloseIcon from 'react-icons/lib/fa/times-circle';
-import { Link } from 'react-router-dom';
+import CloseIcon from "react-icons/lib/fa/times-circle";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 class Contact extends Component {
@@ -13,30 +13,22 @@ class Contact extends Component {
       message: "",
       sent: false
     };
-    this.handleSend = this.handleSend.bind(this);
-    this.handleInputChangeName = this.handleInputChangeName.bind(this);
-    this.handleInputChangeEmail = this.handleInputChangeEmail.bind(this);
-    this.handleInputChangeSubject = this.handleInputChangeSubject.bind(this);
-    this.handleInputChangeMessage = this.handleInputChangeMessage.bind(this);
   }
 
-  handleSend() {
+  handleSend = () => {
     var { name, email, subject, message, sent } = this.state;
-    if (validateEmail(email)){
-
-      axios
-        .post(
-          `/api/sendEmail/?name=${name}&email=${email}&subject=${subject}&message=${message}`
-        )
-        .then(res => {
-          console.log(res.data);
-        });
-    }
+    axios
+      .post(
+        `/api/sendEmail/?name=${name}&email=${email}&subject=${subject}&message=${message}`
+      )
+      .then(res => {
+        // console.log(res.data);
+      });
   }
 
   resetState = () => {
     if (!this.state.sent) {
-      console.log(this.state);
+      // console.log(this.state);
       this.setState({
         name: "",
         email: "",
@@ -48,65 +40,68 @@ class Contact extends Component {
   };
 
   handleClick = () => {
-    console.log("handleClick triggered--->");
+    // console.log("handleClick triggered--->");
     this.handleSend();
     this.resetState();
     window.location.reload();
   };
-  handleInputChangeName(value) {
-    this.setState({ name: value });
+
+  handleInput = (e) => {
+    // console.log("e--->", e.target.name);
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   }
-  handleInputChangeEmail(value) {
-    this.setState({ email: value });
-  }
-  handleInputChangeSubject(value) {
-    this.setState({ subject: value });
-  }
-  handleInputChangeMessage(value) {
-    this.setState({ message: value });
-  }
+
   render() {
-    console.log("state--->", this.state);
+    // console.log("state--->", this.state);
     return (
       <div className="contact-container">
-        <div className='xBtn-container'>
-            <Link to='/'><button className='xBtn'><CloseIcon/></button></Link>
+        <div className="xBtn-container">
+          <Link to="/">
+            <button className="xBtn">
+              <CloseIcon />
+            </button>
+          </Link>
         </div>
         <h1 className="header">Contact Form</h1>
-          <span className="contact-form">
-            <input
-              className="name"
-              placeholder="Name:"
-              onChange={e => this.handleInputChangeName(e.target.value)}
-            />
-            <input
-              className="email"
-              placeholder="Email:"
-              onChange={e => this.handleInputChangeEmail(e.target.value)}
-            />
-            <input
-              className="subject"
-              placeholder="Subject:"
-              onChange={e => this.handleInputChangeSubject(e.target.value)}
-            />
-            <textarea
-              rows="4"
-              cols="50"
-              className="message"
-              placeholder="Message:"
-              onChange={e => this.handleInputChangeMessage(e.target.value)}
-            />
-            <button className="send" onClick={this.handleClick}>
-              <h2 className="submitBtn">Submit</h2>
-            </button>
-          </span>
+        <span className="contact-form">
+          <input
+            className="name"
+            placeholder="Name:"
+            type="text"
+            name="name"
+            onChange={this.handleInput}
+          />
+          <input
+            className="email"
+            placeholder="Email:"
+            type="text"
+            name="email"
+            onChange={this.handleInput}
+          />
+          <input
+            className="subject"
+            placeholder="Subject:"
+            type="text"
+            name="subject"
+            onChange={this.handleInput}
+          />
+          <input
+            className="message"
+            placeholder="Message:"
+            type="text"
+            name="message"
+            onChange={this.handleInput}
+          />
+
+          <button className="send" onClick={this.handleClick}>
+            <h2 className="submitBtn">Submit</h2>
+          </button>
+        </span>
       </div>
     );
   }
 }
 
 export default Contact;
-
-export function validateEmail(email){
-
-}
